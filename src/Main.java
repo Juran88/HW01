c0ad0362e898f9666fef3b6ca8b30603d75b611f
Juran88 <juriaganits@gmail.com>
2023-09-20 12:55:52 +0300

    my first homework

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Say your name");
+        String name = scanner.nextLine();
+
+        System.out.println("Welcom" + ","+ name);
+
+
+    }
+}
